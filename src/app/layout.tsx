edb050a67a327957d4292611d3be4a750d02568c
Nikhil Bhadait <codesharad@gmail.com>
2025-04-01 02:50:07 +0530

    added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%2210 0 100 100%22><text y=%22.90em%22 font-size=%2290%22>⚡</text></svg>"></link>
+        <link rel="icon" href="/faicon.png" sizes="any" />
       </head>
 
       <body
